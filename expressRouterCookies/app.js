9a32be924fe409afd81bf45734d9b863ad42524a
Fergus <intel4004u@gmail.com>
2022-09-28 18:14:10 +0100

    57. Signing Cookies

--- expressRouterCookies/app.js
+++ expressRouterCookies/app.js
@@ -2,13 +2,11 @@ const express = require('express')
 const app = express()
 const shelterRoutes = require('./myRoutes/shelters')
 const adminRoutes = require('./myRoutes/admin')
-const cookieParser = require('cookie-parser')
 
 
 // Only requests to these matching prefixes will be sent to the respective router
 app.use('/shelter', shelterRoutes)
 app.use('/admin', adminRoutes)
-app.use(cookieParser())
 
 app.listen(3005, () => {
   console.log('I am listening on port: 3005')
--- expressRouterCookies/myRoutes/admin.js
+++ expressRouterCookies/myRoutes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const adminRouter = express.Router()
+const cookieParser = require('cookie-parser')
 
+adminRouter.use(cookieParser('forSigningCookies'))
 adminRouter.use((req, res, next) => {
   if (req.query.isAdmin) {
     next()
@@ -10,7 +12,13 @@ adminRouter.use((req, res, next) => {
 })
 
 adminRouter.get('/', (req, res) => {
-  res.send("You are an admin")
+  res.cookie('Barbed Wire', 'Yes', { signed: true })
+  res.cookie('notASignedCookie', 101)
+  // This won't display signed cookies
+  console.log(req.cookies)
+  // This will display signed cookies
+  console.log(req.signedCookies)
+  res.send("You are an admin, and cookie has been signed")
 })
 
 module.exports = adminRouter
