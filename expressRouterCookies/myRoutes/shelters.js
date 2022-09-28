b7e0b8a6fafb890dda3e9c8b3175d73571e2adeb
Fergus <intel4004u@gmail.com>
2022-09-28 16:52:46 +0100

    56. Cookie Parser Middleware

--- expressRouterCookies/myRoutes/shelters.js
+++ expressRouterCookies/myRoutes/shelters.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 /* A router object is an isolated instance of middleware and routes. You can think of it as a “mini-application,” capable only of performing middleware and routing functions. */
 const shelterRouter = express.Router();
+
+shelterRouter.use(cookieParser())
 
 // Any route ending in the following suffixes will be matched
 shelterRouter.get('/', (req, res) => {
   res.cookie('name', 'Fergus')
   res.cookie('animal', 'Harlequin')
+  res.cookie('music', 'Hot Chip')
   res.send("View All Shelters: Cookie set")
 })
 
 shelterRouter.get('/:id', (req, res) => {
+  console.log(req.cookies)
   res.send("View a specific Shelter")
 })
 
